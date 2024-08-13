63c01a38d89b5a561a43cd4997de5e6256817f30
famgz <famgz@proton.me>
2024-08-13 17:03:10 -0300

    feat: increment track card

--- src/app/tracks/_components/track-card.tsx
+++ src/app/tracks/_components/track-card.tsx
@@ -1,24 +1,27 @@
 import ChartIcon from "@/components/icons/chart";
 import { buttonVariants } from "@/components/ui/button";
 import { HoverCard, HoverCardContent } from "@/components/ui/hover-card";
+import { baseAssetsUrl } from "@/constants/api";
 import { cn } from "@/lib/utils";
 import { Prisma } from "@prisma/client";
 import { HoverCardTrigger } from "@radix-ui/react-hover-card";
-import { ClockIcon, DotIcon, PlayIcon } from "lucide-react";
+import { ClockIcon, DotIcon, LandmarkIcon, PlayIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Fragment } from "react";
 
 interface Props {
   track: Prisma.TrackGetPayload<{
     include: {
       skills: true;
+      corporate: true;
     };
   }>;
 }
 
 export default function TrackCard({ track }: Props) {
   return (
-    <HoverCard>
+    <HoverCard openDelay={1000} closeDelay={0}>
       <HoverCardTrigger asChild>
         <Link
           href={`/tracks/${track.slug}`}
@@ -33,52 +36,72 @@ export default function TrackCard({ track }: Props) {
           />
         </Link>
       </HoverCardTrigger>
-      <HoverCardContent className="flex flex-col gap-2.5" sideOffset={-50}>
-        <h3 className="line-clamp-2 text-sm font-bold">{track.name}</h3>
-
-        <div className="flex items-center">
-          <div className="flex flex-1 items-center gap-3 font-light">
-            <div className="flex items-end gap-1">
-              <ChartIcon fontSize={14} />
-              <span className="text-[10px] leading-none">
-                Nível {track.level}
-              </span>
-            </div>
-
-            <div className="flex items-end gap-1">
-              <ClockIcon size={14} />
-              <span className="text-[10px] leading-none">
-                {track.workload}h
-              </span>
-            </div>
-          </div>
-
-          <Link
-            className={cn(
-              buttonVariants({ variant: "ghost", size: "icon" }),
-              "size-7 rounded-full bg-foreground",
-            )}
-            href={`/tracks/${track.slug}`}
-          >
-            <PlayIcon
-              className="fill-background stroke-none hover:fill-foreground"
-              size={18}
-            />
-          </Link>
+      <HoverCardContent className="flex items-center gap-4" sideOffset={-50}>
+        <div className="relative size-24 shrink-0">
+          <Image
+            src={`${baseAssetsUrl}/${track.badge}`}
+            fill
+            alt={track.name}
+            className="object-contain"
+            sizes="100px"
+          />
         </div>
 
-        <div className="flex flex-wrap items-center gap-x-1 gap-y-0">
-          {track.skills.map((s, i, a) => (
-            <>
-              <span className="text-[10px] font-semibold" key={s.id}>
-                {s.name}
-              </span>
+        <div className="flex flex-1 flex-col gap-2">
+          <h3 className="line-clamp-2 text-sm font-bold">{track.name}</h3>
 
-              {i < a.length - 1 && (
-                <DotIcon className="text-muted-foreground" />
+          <div className="flex items-center gap-2">
+            <div className="flex flex-1 items-center gap-3 font-light">
+              {/* level */}
+              <div className="flex items-end gap-1">
+                <ChartIcon fontSize={14} />
+                <span className="whitespace-nowrap text-[10px] leading-none">
+                  Level {track.level}
+                </span>
+              </div>
+
+              {/* workload */}
+              <div className="flex items-end gap-1">
+                <ClockIcon size={14} />
+                <span className="text-[10px] leading-none">
+                  {track.workload}h
+                </span>
+              </div>
+
+              {/* corporate */}
+              <div className="flex items-end gap-1">
+                <LandmarkIcon size={14} />
+                <span className="text-[10px] leading-none">
+                  {track.corporate.name}
+                </span>
+              </div>
+            </div>
+
+            <Link
+              className={cn(
+                buttonVariants({ variant: "ghost", size: "icon" }),
+                "size-7 rounded-full bg-foreground",
               )}
-            </>
-          ))}
+              href={`/tracks/${track.slug}`}
+            >
+              <PlayIcon
+                className="fill-background stroke-none hover:fill-foreground"
+                size={18}
+              />
+            </Link>
+          </div>
+
+          <div className="flex flex-wrap items-center">
+            {track.skills.map((s, i, a) => (
+              <Fragment key={s.id}>
+                <span className="text-[10px] font-semibold">{s.name}</span>
+
+                {i < a.length - 1 && (
+                  <DotIcon className="text-muted-foreground" />
+                )}
+              </Fragment>
+            ))}
+          </div>
         </div>
       </HoverCardContent>
     </HoverCard>
