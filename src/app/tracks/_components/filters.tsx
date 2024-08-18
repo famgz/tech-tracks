dafcfb716be627b7d794220d90964fe9792ba818
famgz <famgz@proton.me>
2024-08-17 22:37:11 -0300

    feat: ensure tracks will be filtered by URL searchparams

--- src/app/tracks/_components/filters.tsx
+++ src/app/tracks/_components/filters.tsx
@@ -1,30 +1,41 @@
 "use client";
 
+import { IFilters } from "@/app/tracks/page";
 import { Checkbox } from "@/components/ui/checkbox";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
-import { Career, Corporate, Skill } from "@prisma/client";
-import { SearchIcon } from "lucide-react";
+import { SearchIcon, XIcon } from "lucide-react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { ChangeEvent } from "react";
 import { useDebouncedCallback } from "use-debounce";
 
 interface Props {
-  filters: {
-    level: { id: string; name: string }[];
-    career: Career[];
-    corporate: Corporate[];
-    skill: Skill[];
-  };
+  filters: IFilters;
   className?: string;
 }
 
+type FilterKeys = keyof Props["filters"];
+
+type InitialSearchParams = {
+  [K in FilterKeys]: string[];
+};
+
 export default function Filters({ filters, className }: Props) {
   const searchParams = useSearchParams();
+  const initialSearchParams: InitialSearchParams = {
+    level: searchParams.getAll("level"),
+    career: searchParams.getAll("career"),
+    corporate: searchParams.getAll("corporate"),
+    skill: searchParams.getAll("skill"),
+  };
   const pathname = usePathname();
   const { replace } = useRouter();
 
   const params = new URLSearchParams(searchParams);
+
+  function updateUrl() {
+    replace(`${pathname}?${params.toString()}`);
+  }
 
   const handleInputChange = useDebouncedCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
@@ -34,12 +45,16 @@ export default function Filters({ filters, className }: Props) {
       } else {
         params.delete("search");
       }
-      replace(`${pathname}?${params.toString()}`);
+      updateUrl();
     },
     500,
   );
 
-  function handleCheckboxChange(key: string, value: string, checked: boolean) {
+  function handleCheckboxChange(
+    key: FilterKeys,
+    value: string,
+    checked: boolean,
+  ) {
     if (checked) {
       params.append(key, value);
     } else {
@@ -56,50 +71,66 @@ export default function Filters({ filters, className }: Props) {
         }
       });
     }
-    replace(`${pathname}?${params.toString()}`);
+    updateUrl();
+  }
+
+  function handleResetInput() {
+    params.delete("search");
+    updateUrl();
   }
 
   return (
-    <div className={cn("flex max-w-[20%] flex-col", className)}>
+    <div className={cn("flex max-w-[min(20vw,250px)] flex-col", className)}>
       <span className="mb-2">Filters</span>
-      <div className="flex-center mb-4 gap-2 rounded-lg border px-2 py-1">
+      <form className="flex-center mb-4 gap-2 rounded-lg border px-2 py-1">
         <SearchIcon className="size-4 text-muted-foreground" />
         <input
-          className="flex-1 bg-transparent outline-none"
+          className="flex-1 bg-transparent text-sm outline-none"
           size={1}
           onChange={handleInputChange}
         />
-      </div>
+        <button type="reset" onClick={handleResetInput}>
+          <XIcon size={12} className="text-muted-foreground" />
+        </button>
+      </form>
       <ScrollArea className="-mr-2 h-[100px] flex-auto pr-4">
-        <div className="space-y-3">
-          {Object.entries(filters).map(([k, v]) => (
-            <div key={k}>
-              <h3 className="mb-1 text-sm font-bold capitalize">{k}</h3>
-              <div className="">
-                {v.map((x) => (
-                  <div
-                    key={x.id as string}
-                    className="ml-2 flex items-center gap-2 rounded py-0.5 hover:bg-muted"
-                    title={x.name}
-                  >
-                    <Checkbox
-                      id={x.name}
-                      name={x.name}
-                      onCheckedChange={(checked) =>
-                        handleCheckboxChange(k, x.id, !!checked)
-                      }
-                    />
-                    <label
-                      htmlFor={x.name}
-                      className="flex-1 cursor-pointer whitespace-nowrap text-xs"
+        <div className="space-y-3 overflow-x-hidden">
+          {Object.entries(filters).map(([k, v]) => {
+            const key = k as FilterKeys;
+
+            return (
+              <div key={key}>
+                <h3 className="mb-1 text-sm font-bold capitalize">{k}</h3>
+                <div className="">
+                  {v.map((x: any) => (
+                    <div
+                      key={x.id as string}
+                      className="ml-2 flex items-center gap-2 rounded py-0.5 hover:bg-muted"
+                      title={x.name}
                     >
-                      {x.name}
-                    </label>
-                  </div>
-                ))}
+                      <Checkbox
+                        id={x.name}
+                        name={x.name}
+                        defaultChecked={initialSearchParams[key].includes(x.id)}
+                        onCheckedChange={(checked) =>
+                          handleCheckboxChange(key, x.id, !!checked)
+                        }
+                      />
+                      <label
+                        htmlFor={x.name}
+                        className="flex flex-1 cursor-pointer items-center gap-1 truncate whitespace-nowrap text-xs"
+                      >
+                        <span className="inline-block max-w-[min(10vw,134px)] truncate">
+                          {x.name}
+                        </span>
+                        <span>({x._count?.tracks ?? 0})</span>
+                      </label>
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </ScrollArea>
     </div>
--- src/app/tracks/page.tsx
+++ src/app/tracks/page.tsx
@@ -3,16 +3,24 @@ import TrackCard from "@/app/tracks/_components/track-card";
 import { auth } from "@/auth";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { db } from "@/lib/prisma";
-import { SearchIcon } from "lucide-react";
+import { Prisma } from "@prisma/client";
 import { redirect } from "next/navigation";
 
 interface Props {
   searchParams: {
-    skill: string;
-    level: string;
-    corporate: string;
+    search: string;
     career: string;
+    corporate: string;
+    level: string;
+    skill: string;
   };
+}
+
+export interface IFilters {
+  career: Prisma.CareerGetPayload<{ include: { _count: true } }>[];
+  corporate: Prisma.CorporateGetPayload<{ include: { _count: true } }>[];
+  level: { id: string; name: string }[];
+  skill: Prisma.SkillGetPayload<{ include: { _count: true } }>[];
 }
 
 export default async function TracksPage({ searchParams }: Props) {
@@ -23,16 +31,19 @@ export default async function TracksPage({ searchParams }: Props) {
   }
 
   const [skill, career, corporate, tracks] = await Promise.all([
-    db.skill.findMany({ orderBy: { name: "asc" } }),
-    db.career.findMany({ orderBy: { name: "asc" } }),
-    db.corporate.findMany({ orderBy: { name: "asc" } }),
+    db.skill.findMany({ orderBy: { name: "asc" }, include: { _count: true } }),
+    db.career.findMany({ orderBy: { name: "asc" }, include: { _count: true } }),
+    db.corporate.findMany({
+      orderBy: { name: "asc" },
+      include: { _count: true },
+    }),
     db.track.findMany({
-      take: 100,
-      include: { skills: true, corporate: true },
+      // take: 100,
+      include: { skills: true, corporate: true, careers: true },
     }),
   ]);
 
-  const filters = {
+  const filters: IFilters = {
     level: [
       { id: "1", name: "Iniciante" },
       { id: "2", name: "Intermediário" },
@@ -43,24 +54,68 @@ export default async function TracksPage({ searchParams }: Props) {
     skill,
   };
 
+  const normalizeParams = (param: string | string[]) =>
+    Array.isArray(param) || param === undefined ? param : [param];
+
+  const parsedSearchParams = {
+    search: searchParams.search,
+    career: normalizeParams(searchParams.career),
+    corporate: normalizeParams(searchParams.corporate),
+    level: normalizeParams(searchParams.level),
+    skill: normalizeParams(searchParams.skill),
+  };
+
+  const filteredTracks = Object.values(parsedSearchParams).every(
+    (x) => x === undefined,
+  )
+    ? tracks
+    : tracks.filter(
+        (t) =>
+          // search
+          (!parsedSearchParams.search ||
+            t.name.toLowerCase().includes(parsedSearchParams.search)) &&
+          // level
+          (!parsedSearchParams.level ||
+            parsedSearchParams.level.includes(t.level.toString())) &&
+          // skill
+          (!parsedSearchParams.skill ||
+            t.skills.some((skill) =>
+              parsedSearchParams.skill.includes(skill.id),
+            )) &&
+          // career
+          (!parsedSearchParams.career ||
+            t.careers.some((career) =>
+              parsedSearchParams.career.includes(career.id),
+            )) &&
+          // corporate
+          (!parsedSearchParams.corporate ||
+            parsedSearchParams.corporate.includes(t.corporate.id)),
+      );
+
   return (
     <div className="_container flex flex-col py-2">
       <h1 className="py-2 text-center text-3xl font-bold">Tracks</h1>
 
-      <div className="flex flex-1 gap-4">
+      <div className="flex flex-1 gap-6">
         {/* Filters column */}
         <Filters filters={filters} className="max-sm:hidden" />
 
         {/* Cards column */}
         <div className="flex flex-1 flex-col">
-          <span className="mb-2">191 tracks</span>
-          <ScrollArea className="-mr-2 h-[100px] flex-auto pr-4">
-            <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-              {tracks.map((t) => (
-                <TrackCard track={t} key={t.id} />
-              ))}
+          <span className="mb-2">{filteredTracks.length} tracks</span>
+          {filteredTracks.length > 0 ? (
+            <ScrollArea className="-mr-2 h-[100px] flex-auto pr-4">
+              <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                {filteredTracks.map((t) => (
+                  <TrackCard track={t} key={t.id} />
+                ))}
+              </div>
+            </ScrollArea>
+          ) : (
+            <div className="flex-center h-full w-full text-xl text-muted-foreground">
+              No results
             </div>
-          </ScrollArea>
+          )}
         </div>
       </div>
     </div>
