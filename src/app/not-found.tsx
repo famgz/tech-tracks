fc6db41b527c7e54ea69f63c2a73afbf7b6a0d54
famgz <famgz@proton.me>
2024-09-20 13:28:10 -0300

    feat: add not found illustration

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,29 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function NotFoundPage() {
   return (
     <div className="flex-center h-full w-full">
       <div className="flex-center">
-        <div className="space-y-1 border-r px-12">
+        <div className="flex flex-col items-center border-r px-12">
           <p className="text-4xl font-bold">404</p>
-          <h1 className="text-2xl font-light">Not found</h1>
+
+          <Image
+            src={"/not-found.svg"}
+            width={200}
+            height={200}
+            alt="not found image showing a bug inside a laptop screen"
+          />
+          <h1 className="text-2xl font-light">
+            Este recurso não foi encontrado
+          </h1>
         </div>
 
         <div className="px-12">
           <Button size={"lg"}>
             <Link href={"/"} className="text-lg !font-bold">
-              Back to Home
+              Voltar para Home
             </Link>
           </Button>
         </div>
