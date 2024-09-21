90091799a6d5dd84f0097f2df999a6e9792fefd8
famgz <famgz@proton.me>
2024-09-21 14:49:06 -0300

    fix: ensure lessons list will scroll in course page

--- src/app/course/[id]/page.tsx
+++ src/app/course/[id]/page.tsx
@@ -1,4 +1,6 @@
 import LessonsAccordion from "@/app/course/[id]/_components/lessons-accordion";
+import BackButton from "@/components/buttons/back";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { db } from "@/lib/prisma";
 import { YoutubeIcon } from "lucide-react";
 import { notFound } from "next/navigation";
@@ -7,9 +9,10 @@ interface Props {
   params: {
     id: string;
   };
+  searchParams: { back: string };
 }
 
-export default async function CoursePage({ params }: Props) {
+export default async function CoursePage({ params, searchParams }: Props) {
   const { id } = params;
 
   const course = await db.course.findUnique({
@@ -23,20 +26,23 @@ export default async function CoursePage({ params }: Props) {
 
   return (
     <div className="mx-auto flex size-full max-w-[1920px] flex-col">
-      <div className="grid h-full grid-cols-[3fr_1fr]">
+      <div className="grid h-full grid-cols-[2fr_1fr]">
         <div className="flex-center flex-col border-r">
-          <h1 className="w-full px-5 py-3 text-left text-xl">
-            {course.name} - {course.lessons[0].contents[0].name}
-          </h1>
+          <div className="flex w-full items-center justify-start gap-3 px-5 py-3">
+            <BackButton backUrl={searchParams.back} />
+            <h1 className="text-xl">{course.name}</h1>
+          </div>
           <YoutubeIcon
             className="size-44 flex-1 text-muted-foreground"
             strokeWidth={1}
           />
         </div>
 
-        <div className="">
+        <div className="flex h-full flex-col">
           <h1 className="px-5 py-3 text-xl text-muted-foreground">Lições</h1>
-          <LessonsAccordion lessons={course.lessons} />
+          <ScrollArea className="h-[100px] flex-auto">
+            <LessonsAccordion lessons={course.lessons} />
+          </ScrollArea>
         </div>
       </div>
     </div>
