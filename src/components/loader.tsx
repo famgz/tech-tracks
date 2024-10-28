22d2ee7ef836084c9f813cc2e3acf878ef6062b1
famgz <famgz@proton.me>
2024-10-28 14:43:39 -0300

    feat: add text to loader

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -2,8 +2,9 @@ import { LoaderCircleIcon } from "lucide-react";
 
 export default function Loader() {
   return (
-    <div className="flex-center size-full flex-1 bg-background/20">
+    <div className="flex-center size-full flex-1 flex-col gap-1 bg-background/20">
       <LoaderCircleIcon className="size-12 animate-spin stroke-[2px] text-muted-foreground" />
+      <span className="text-muted-foreground">Carregando...</span>
     </div>
   );
 }
