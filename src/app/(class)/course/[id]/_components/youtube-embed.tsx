19a499034631ab9b3aa96052374c451647c332db
Famgz <famgz@proton.me>
2024-11-01 15:30:05 -0300

    chore: move component to its group folder fix: ensure video frame occupy available space

--- src/app/(class)/course/[id]/_components/youtube-embed.tsx
+++ src/app/(class)/course/[id]/_components/youtube-embed.tsx
@@ -5,7 +5,8 @@ export default function YouTubeEmbed({ videoId }: { videoId: string }) {
     <div className="relative flex w-full flex-1 items-center">
       <iframe
         src={`https://www.youtube.com/embed/${videoId}?rel=0`}
-        className="aspect-video w-full"
+        width='100%'
+        height='100%'
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
